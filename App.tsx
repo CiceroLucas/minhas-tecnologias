ae05b31d15270cf52fa3656beb06aba556620b34
CiceroLucas <lukasferreiradesousa890@gmail.com>
2023-10-20 13:48:02 -0300

    first screen

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
 import { useFonts, Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter';
+import { theme } from './src/theme';
+import { HomeScreen } from './src/screens';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -9,7 +12,7 @@ export default function App() {
   })
   return (
     <View style={styles.container}>
-      {!fontsLoaded ? <Text>carregou</Text> : <ActivityIndicator size={50}/>}
+      {fontsLoaded ? <HomeScreen/> : <Loading/>}
       
       <StatusBar style="auto" />
     </View>
@@ -19,7 +22,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: theme.colors.base.gray600,
     alignItems: 'center',
     justifyContent: 'center',
   },
