c223ce860fb07344909cb4b6becbfcbeeb562c0e
CiceroLucas <lukasferreiradesousa890@gmail.com>
2023-10-22 14:00:49 -0300

    feat: add func task

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,13 @@ import {Text, View, TextInput, TouchableOpacity} from 'react-native'
 import { styles } from "./styles";
 import { theme } from '../../theme';
 
-export function Header(){
+type HeaderProps = {
+    task: string,
+    onChangeText: (task: string) => void
+    onPress: () => void
+}
+
+export function Header({task, onChangeText, onPress}: HeaderProps){
     return (
     <View style={styles.headerContainer}>
        <Text style={styles.title}>Minhas Tecnologias</Text>
@@ -10,8 +16,10 @@ export function Header(){
             <TextInput style={styles.input} 
                        placeholder='Adicione uma nova tecnologia'
                        placeholderTextColor={theme.colors.base.gray300}
+                       value={task}
+                       onChangeText={onChangeText}
                        />
-            <TouchableOpacity style={styles.button}>
+            <TouchableOpacity style={styles.button} onPress={onPress}>
                 <Text>+</Text>
             </TouchableOpacity>
        </View>
